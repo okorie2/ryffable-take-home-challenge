b4829486b43a85545656fc6d0174a334f9ff75cb
Godswill Edet <97456640+underscoreDev@users.noreply.github.com>
2022-03-18 20:20:04 +0100

    feat: final commit

---
+++ components/table.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { MockDataProps } from "../utils/data";
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+interface MockData {
+  mockData: MockDataProps[];
+}
+
+export default function StickyHeadTable({ mockData }: MockData) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) =>
+    setPage(newPage);
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell align="center">Name</StyledTableCell>
+              <StyledTableCell align="center">Place</StyledTableCell>
+              <StyledTableCell align="center">Animal</StyledTableCell>
+              <StyledTableCell align="center">Food</StyledTableCell>
+              <StyledTableCell align="center">Thing</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {mockData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{row.place}</StyledTableCell>
+                  <StyledTableCell align="center">{row.animal}</StyledTableCell>
+                  <StyledTableCell align="center">{row.food}</StyledTableCell>
+                  <StyledTableCell align="center">{row.thing}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={mockData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,159 +1,51 @@
 import axios from "axios";
-import  React ,{ useEffect, useState, ChangeEvent } from "react";
-import right from "../public/svgs/arrow-right.svg";
-import left from "../public/svgs/arrow-left.svg";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import { MockDataProps } from "../utils/data";
+import DataTable from "../components/table";
 
-interface  Table{
-  
-    name: string,
-    place: string,
-    animal: string,
-    food: string,
-    thing: string,
-  
-}
 export default function Home() {
+  const [mockData, setMockData] = useState<MockDataProps[]>([]);
+  const [filteredData, setFilteredData] = useState(mockData);
 
-  const [rows, setRows] = useState([]as Array<Table>);
-  const [defaultRows, setDefaultRows] = useState([]as Array<Table>);
-  const [mainRows, setMainRows]  = useState([] as Array<Table>);
   const getData = async () => {
-    const response = await axios.get("/api/table_data");
-    console.log(response.data);
-    setMainRows(response.data?.data);
+    const { data } = await axios.get("/api/table_data");
+    setMockData(data?.data);
+    setFilteredData(data?.data);
   };
 
-  const handleSearch = (e:ChangeEvent<HTMLInputElement>)=>{
-    setDefaultRows(
-      mainRows.filter(
-        (item) =>
-          item.name
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase()) ||
-          item.place
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase()) ||
-          item.animal
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase()) ||
-          item.food
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase()) ||
-          item.thing
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase())
-      )
-    )
-  }
-
-  const pageLength = 10;
-
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const setPagination = () => {
-    let pageNumbers = 0;
-    if (defaultRows.length % pageLength === 0) {
-      pageNumbers = defaultRows.length / pageLength;
-    } else {
-      pageNumbers = defaultRows.length / pageLength + 1;
-    }
-    const numbers = [];
-    let x = 1;
-    while (x < pageNumbers) {
-      numbers.push(x);
-      x += 1;
-    }
-    return { pageNumbers, numbers };
-  };
-
-  const { pageNumbers, numbers } = setPagination();
-
-  useEffect(() => {
-    setDefaultRows(mainRows);
-  }, [mainRows]);
-  useEffect(() => {
-    if (defaultRows.length > pageLength) {
-      listSlicer(1);
-    } else {
-      setRows(defaultRows);
-    }
-  }, [defaultRows]);
-
-  const listSlicer = (pageNo) => {
-    if (pageNo > 0 && pageNo <= pageNumbers) {
-      const start = pageNo * pageLength - pageLength;
-      const stop = pageNo * pageLength - 1;
-      setRows(defaultRows.slice(start, stop));
-      setCurrentPage(pageNo);
-    }
-  };
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <>
-      <div className="p-3">
-        <div className="mb-3">
-          <input
-            type="text"
-            className="outline-none p-2 border-2 border-[#dddddd]"
-            placeholder="Search"
-            onChange={
-             handleSearch
-            }
-          />
-        </div>
-        <table className="text-left w-full border-collapse font-nunito">
-          <tbody>
-            <tr className="even:bg-[#dddddd]">
-              <th className="border-[#dddddd] p-2 border-2">Name</th>
-              <th className="border-[#dddddd] p-2 border-2">Place</th>
-              <th className="border-[#dddddd] p-2 border-2">Animal</th>
-              <th className="border-[#dddddd] p-2 border-2">Food</th>
-              <th className="border-[#dddddd] p-2 border-2">Thing</th>
-            </tr>
+    <div className="mb-3 mt-5">
+      <input
+        type="text"
+        className="outline-none p-2  border-2 border-[#dddddd]"
+        placeholder="Search"
+        onChange={(e) =>
+          setFilteredData(
+            mockData.filter(
+              (item) =>
+                item.name
+                  .toLowerCase()
+                  .includes(e.target.value.toLowerCase()) ||
+                item.place
+                  .toLowerCase()
+                  .includes(e.target.value.toLowerCase()) ||
+                item.animal
+                  .toLowerCase()
+                  .includes(e.target.value.toLowerCase()) ||
+                item.food
+                  .toLowerCase()
+                  .includes(e.target.value.toLowerCase()) ||
+                item.thing.toLowerCase().includes(e.target.value.toLowerCase())
+            )
+          )
+        }
+      />
 
-            {rows.map((row, index) => (
-              <tr key={index} className="even:bg-[#dddddd]">
-                <td className="border-[#dddddd] p-2 border-2">{row.name}</td>
-                <td className="border-[#dddddd] p-2 border-2">{row.place}</td>
-                <td className="border-[#dddddd] p-2 border-2">{row.animal}</td>
-                <td className="border-[#dddddd] p-2 border-2">{row.food}</td>
-                <td className="border-[#dddddd] p-2 border-2">{row.thing}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="py-3 ">
-          <div className="flex justify-center">
-            <div
-              className="flex items-center shadow-lg  w-max hover:shadow-inner cursor-pointer"
-              onClick={() => listSlicer(currentPage - 1)}
-            >
-              <Image src={left} alt="" width={30} height={30} />
-            </div>
-            {numbers.map((number, index) => (
-              <div
-                key={index}
-                className={`flex items-center shadow-lg  w-max hover:shadow-inner cursor-pointer p-3 ${
-                  currentPage === number && "shadow-inner"
-                }`}
-                onClick={() => listSlicer(number)}
-              >
-                {number}
-              </div>
-            ))}
-            <div
-              className="flex items-center shadow-lg w-max hover:shadow-inner cursor-pointer"
-              onClick={() => listSlicer(currentPage + 1)}
-            >
-              <Image src={right} alt="" width={30} height={30} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      <DataTable mockData={filteredData} />
+    </div>
   );
 }
